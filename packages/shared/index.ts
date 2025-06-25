2d39da2c033c91658e5968e01a25baf7d12e2257
Sorin0404 <haewong27@gmail.com>
2025-06-25 22:45:51 +0900

    yarn workspace monorepo shadcn 적용기 - 적용하기 위해 shadcn 설치 했으나 적용이 안됨. - 결론은 tailwindcss를 각각 프로젝트에 설치하는 것이 필요

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -1,3 +1,10 @@
 export * from "./src/utils/static/hello";
 
 export { HelloWorld } from "./src/components/HelloWorld";
+
+export * from "./src/components/ui/button";
+export * from "./src/components/ui/dialog";
+export * from "./src/components/ui/popover";
+export * from "./src/components/ui/command";
+
+export * from "./src/lib/utils";
